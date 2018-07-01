4022dda16077638d207a5b7c67541e53fdece2f7
hanshungithub <hshanshun@163.com>
2018-07-01 14:35:02 +0800

    annotation

--- src/main/java/cn/hassan/annotation/t1/MethodAnnotations.java
+++ src/main/java/cn/hassan/annotation/t1/MethodAnnotations.java
@@ -1,5 +1,6 @@
 package cn.hassan.annotation.t1;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 /**
@@ -16,7 +17,28 @@ public class MethodAnnotations {
 
     public static void main(String[] args) throws Exception {
 
-        Class<?> clazz = Class.forName("MethodAnnotations");
+        Class<?> clazz = Class.forName("cn.hassan.annotation.t1.MethodAnnotations");
         Method method = clazz.getMethod("hello",String.class, String.class);
+        Annotation[][] annotations = method.getParameterAnnotations();
+        String param1 = null;
+        String param2 = null;
+        for (int i = 0; i < annotations.length; i++) {
+            System.out.println("annotation for parpmter" + (i + 1));
+            Annotation[] annotation = annotations[i];
+            for (Annotation anno : annotation) {
+                if (anno instanceof QueryParam) {
+                    QueryParam queryParam = (QueryParam) anno;
+                    param1 = queryParam.value();
+                    System.out.println(queryParam.annotationType().getSimpleName() + " --- " + param1);
+                }else {
+                    DefaultValue defaultValue = (DefaultValue) anno;
+                    param2 = defaultValue.value();
+                    System.out.println(defaultValue.annotationType().getSimpleName() + " --- " + param2);
+                }
+            }
+
+        }
+        Object o = clazz.newInstance();
+        method.invoke(o, param1, param2);
     }
 }
