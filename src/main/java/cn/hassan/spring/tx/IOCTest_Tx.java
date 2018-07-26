9c996190e0602ea207a570e37fb683fd76d9ea4b
hanshungithub <hshanshun@163.com>
2018-07-26 18:45:01 +0800

    spring

---
+++ src/main/java/cn/hassan/spring/tx/IOCTest_Tx.java
@@ -0,0 +1,16 @@
+package cn.hassan.spring.tx;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class IOCTest_Tx {
+	
+	@Test
+	public void test01(){
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(TxConfig.class);
+		UserService userService = applicationContext.getBean(UserService.class);
+		userService.insertUser();
+		applicationContext.close();
+	}
+
+}
